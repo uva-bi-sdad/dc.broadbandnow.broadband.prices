5edd8efdd8366f093f38083527c353502304a7e0
Kathryn Linehan <kjl5t@virginia.edu>
2022-01-27 16:27:35 -0500

    doi directory update

--- R/get_data_file_names.R
+++ R/get_data_file_names.R
@@ -8,7 +8,7 @@
 
 get_data_file_names <- function() {
 
-  doi <- readLines("data/dataset_doi")[1]
+  doi <- readLines("dataset_doi")[1]
 
   file_names <- dataverse::dataset_files(
     dataset = doi,
